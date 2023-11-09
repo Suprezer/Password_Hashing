da41b572823e420195c8d50df5e6b655b7bad120
Suprezer <jmo@live.dk>
2023-11-09 20:43:42 +0100

    Added UserController, fixing MVC pattern on new branch.

---
+++ src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,11 @@
+package org.example.controller;
+
+import org.example.model.User;
+
+public class UserController {
+    private User user;
+
+    public UserController(User user) {
+        this.user = user;
+    }
+}
